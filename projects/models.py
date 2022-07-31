1b5da8b5a9988425a6e85df5bfb2d4ba2ecb92db
Your Name <you@example.com>
2022-07-31 13:05:12 +0200

    create and list projects

--- projects/models.py
+++ projects/models.py
@@ -29,7 +29,8 @@ class Project(models.Model):
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
     user = models.ForeignKey(
         AUTH_USER_MODEL,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        null=True
     )
 
     def __str__(self):
